5d7fa5bf54ac97c5fbfc4d2dcd370e1ce3c084a4
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-12-18 23:00:40 +0530

    add navigation to homepage

---
+++ app/index.jsx
@@ -0,0 +1,75 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  ImageBackground,
+  Pressable,
+} from "react-native";
+import icedCoffeeImg from "@/assets/images/iced-coffee.png";
+import { Link } from "expo-router";
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.image}
+        source={icedCoffeeImg}
+        resizeMode="cover"
+      >
+        <Text style={styles.text}>Coffee Shop</Text>
+        <Link style={{ marginHorizontal: "auto" }} href="/explore" asChild>
+          <Pressable style={styles.button}>
+            <Text style={styles.buttonText}>Explore</Text>
+          </Pressable>
+        </Link>
+      </ImageBackground>
+    </View>
+  );
+};
+
+export default app;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: "column",
+  },
+  image: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "rgba(0,0,0,0.5)",
+    marginBottom: 120,
+  },
+  link: {
+    color: "white",
+    fontSize: 42,
+    fontWeight: "bold",
+    textAlign: "center",
+    textDecorationLine: "underline",
+    padding: 4,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  button: {
+    height: 60,
+    borderRadius: 20,
+    justifyContent:"center",
+    backgroundColor: "rgba(0,0,0,0.75)",
+    padding: 6,
+  },
+  buttonText: {
+    color: "white",
+    fontSize: 16,
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 4,
+  },
+});
