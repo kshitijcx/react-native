9f700b4489812c4a46095cc8b4418323557c93f8
Kshitij Chauhan <kshitij.cx@gmail.com>
2024-12-31 23:15:11 +0530

    root layout refactor and theming tweaks, Happy New Year

---
+++ app/menu.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Colors } from "@/constants/Colors";
+const menu = () => {
+  return (
+    <View>
+      <Text>menu</Text>
+    </View>
+  );
+};
+export default menu;
+const styles = StyleSheet.create({});
