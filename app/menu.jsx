c024f51c7a40c39abc58ae9d140f3ca65dea0ac6
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-01-03 22:58:48 +0530

    add menu page barebones and menu data

--- app/menu.jsx
+++ app/menu.jsx
@@ -1,11 +1,35 @@
-import { StyleSheet, Text, View } from "react-native";
+import {
+  Appearance,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  FlatList,
+  Platform,
+} from "react-native";
 import { Colors } from "@/constants/Colors";
 const menu = () => {
+  const colorScheme = Appearance.getColorScheme();
+  const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
+  const styles = createStyles(theme, colorScheme);
+  const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
   return (
-    <View>
-      <Text>menu</Text>
-    </View>
+    <Container>
+      <FlatList
+        data={[{ data: "hello" }, { data: "hello" }, { data: "hello" }]}
+        renderItem={({ item }) => (
+          <View>
+            <Text>{item.data}</Text>
+          </View>
+        )}
+      />
+    </Container>
   );
 };
 export default menu;
-const styles = StyleSheet.create({});
+
+function createStyles(theme, colorScheme) {
+  return StyleSheet.create({});
+}
