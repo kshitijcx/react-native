6a180c6d0ddf8a07fc07adc4791a551653244c61
Kshitij Chauhan <kshitij.cx@gmail.com>
2025-01-04 22:41:22 +0530

    add styles to flatlist container

--- app/menu.jsx
+++ app/menu.jsx
@@ -17,11 +17,21 @@ const menu = () => {
   const theme = colorScheme === "dark" ? Colors.dark : Colors.light;
   const styles = createStyles(theme, colorScheme);
   const Container = Platform.OS === "web" ? ScrollView : SafeAreaView;
+  const separatorCompo = <View style={styles.separator} />;
+  const headerCompo = <Text>Top of List</Text>;
+  const footerCompo = <Text>End of Menu</Text>;
   return (
     <Container>
       <FlatList
         data={MENU_ITEMS}
         keyExtractor={(item) => item.id?.toString()}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={styles.contentContainer}
+        ItemSeparatorComponent={separatorCompo}
+        // ListHeaderComponent={headerCompo}
+        ListFooterComponent={footerCompo}
+        ListFooterComponentStyle={styles.footerComp}
+        ListEmptyComponent={<Text>No Items</Text>}
         renderItem={({ item }) => (
           <View>
             <View>
@@ -38,5 +48,22 @@ const menu = () => {
 export default menu;
 
 function createStyles(theme, colorScheme) {
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    contentContainer: {
+      paddingTop: 10,
+      paddingBottom: 20,
+      paddingHorizontal: 12,
+      backgroundColor: theme.background,
+    },
+    separator: {
+      height: 1,
+      backgroundColor: colorScheme === "dark" ? "papayawhip" : "#000",
+      width: "50%",
+      marginHorizontal: "auto",
+      marginBottom: 10,
+    },
+    footerComp: {
+      marginHorizontal: "auto",
+    },
+  });
 }
